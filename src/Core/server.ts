bc697ac993a6b2b99f2e58d4b70becb3a6006f23
Felipe Piorini <felipe.molica@becompliance.com>
2021-11-07 10:11:23 -0300

    chore: ajustes de indentação

--- src/Core/server.ts
+++ src/Core/server.ts
@@ -1,29 +1,29 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import 'colors';
-var colors = require('colors');
+import "colors";
+var colors = require("colors");
 colors.enable();
 
-import {Express} from "express";
+import { Express } from "express";
 import bodyParser = require("body-parser");
 import cors = require("cors");
 
-let express = require('express');
+let express = require("express");
 
 function startExpress() {
-    let app: Express = express();
+  let app: Express = express();
 
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    var corsOptions = {
-        origin: [/localhost/],
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        credentials: true,
-        optionsSuccessStatus: 200
-    };
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  var corsOptions = {
+    origin: [/localhost/],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
 
-    app.use(cors(corsOptions));
-    return app;
+  app.use(cors(corsOptions));
+  return app;
 }
 
 export const app = startExpress();
--- src/Data/Source/MySQL/Task/TasksModel.ts
+++ src/Data/Source/MySQL/Task/TasksModel.ts
@@ -1,35 +1,37 @@
-import { MySQL} from "@Data/Source/MySQL/MySQL";
-const moment = require('moment-timezone');
+import { MySQL } from "@Data/Source/MySQL/MySQL";
+const moment = require("moment-timezone");
 
 export class TasksModel extends MySQL {
-   
-    protected table: string = 'tasks';
+  protected table: string = "tasks";
 
-    async insertTask(userId: number, description: string, dateEnd: string, status: string) {
+  async insertTask(
+    userId: number,
+    description: string,
+    dateEnd: string,
+    status: string
+  ) {
+    return this.builder.table("tasks").insert({
+      user_id: userId,
+      description: description,
+      date_end: moment(dateEnd)
+        .tz("America/Sao_Paulo")
+        .format("YYYY-MM-DD HH:mm:ss"),
+      status: status,
+    });
+  }
 
-        return this.builder
-            .table('tasks')
-            .insert({
-                user_id: userId,
-                description: description,
-                date_end: moment(dateEnd).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss'),
-                status: status,
-            })
-    } 
+  async getListsTasks() {
+    const query = this.builder
+      .select({
+        id: "T.id",
+        description: "T.description",
+        dateEnd: "T.date_end",
+        userId: "U.id",
+        userName: "U.name",
+      })
+      .from("tasks AS T")
+      .innerJoin("users AS U", "U.id", "T.user_id");
 
-    async getListsTasks() {
-        const query = this.builder
-        .select({
-            id: 'T.id',
-            description: 'T.description',
-            dateEnd: 'T.date_end', 
-            userId: 'U.id',
-            userName: 'U.name'
-        })
-        .from('tasks AS T')
-        .innerJoin('users AS U', 'U.id', 'T.user_id')
-        
-        return this.exec(query);
-    }
-
+    return this.exec(query);
+  }
 }
